7d5114eefe1d12496119b4f156a9325718fa2ec8
MeenuSoni <meenukumari77310@gmail.com>
2025-11-01 16:32:00 +0530

    Cross-platform redirects fix

--- backend/controllers/captchaController.js
+++ backend/controllers/captchaController.js
@@ -11,14 +11,12 @@ export const getCaptcha = (req, res) => {
   const isProduction = process.env.NODE_ENV === "production";
 
   res.cookie("captcha", captcha.text, {
-  httpOnly: true,
-  secure: true,
-  sameSite: "None",
-  domain: ".onrender.com", // ✅ allow sharing across subdomains
-  path: "/",
-  maxAge: 5 * 60 * 1000,
-});
-
+    httpOnly: true,
+    secure: isProduction, 
+    sameSite: isProduction ? "None" : "Lax",
+    path: "/",
+    maxAge: 5 * 60 * 1000,
+  });
    console.log("Generated CAPTCHA:", captcha.text);
 
 
