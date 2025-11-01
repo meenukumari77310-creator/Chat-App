e14ad955a446ddf60d4e7e1f8fc0e02335e0e407
MeenuSoni <meenukumari77310@gmail.com>
2025-11-01 16:40:46 +0530

    Cross-platform redirects fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';   
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Toaster position="top-right" />
-      <App />
-    </HashRouter>
+      <BrowserRouter>
+          <Toaster position="top-right" />
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
